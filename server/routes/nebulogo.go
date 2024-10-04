ca02e7bfbebee21f500780e9fb6f639a27f6eb16
NhProGamer <neo.huyghe@gmail.com>
2024-10-04 11:50:31 +0200

    remove useless code atm

--- server/routes/nebulogo.go
+++ server/routes/nebulogo.go
@@ -1,18 +1,10 @@
 package routes
 
 import (
-	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 )
 
 func NebuloGoApp(c *gin.Context) {
-	claims := jwt.ExtractClaims(c)
-	path := c.DefaultQuery("path", "")
-
-	c.HTML(200, "app.html", gin.H{
-		/*"Items":  items,*/
-		"UserId":     claims["user_id"].(string),
-		"ActualPath": path,
-	})
+	c.HTML(200, "app.html", gin.H{})
 
 }
